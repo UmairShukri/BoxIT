dec67d28775f742415ad6fd525dc61a5afc8a517
Shobian <shobian.20210528@iit.ac.lk>
2023-05-22 15:26:22 +0530

    formating & navbar

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,25 +1,31 @@
 import React from 'react'
 import CountryForm from './Countryform'
 import bgImage from '../images/bg.jpg'
+import NavBar from './NavBar'
 
 
 
 export default function Calculator() {
   return (
-    <div className="flex flex-column  justify-center">
+
+    <div className="flex flex-column ">
       <div className="  flex items-start justify-center h-screen w-80 p-4 border-2 border-gray-500 bg-white bg-cover bg-center"
-       style={{ backgroundImage: `url(${bgImage})`,
-       opacity: '0.9', }} >
-      <p className='font-extrabold'> GET A QUOTE</p>
-      
-        <CountryForm/>
+        style={{
+          backgroundImage: `url(${bgImage})`,
+          opacity: '0.9',
+        }} >
+
+        <p className='font-extrabold'> GET A QUOTE</p>
+
+        <CountryForm />
+        
       </div>
+
       <div className='bg-white'>
-        
-      
-        
+
+
       </div>
-      
+
     </div>
   )
 }
