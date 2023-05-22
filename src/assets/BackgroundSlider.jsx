9fdda2f3304a5ef7c350b9443c3d9bf67303721b
UmairShukri <123258035+UmairShukri@users.noreply.github.com>
2023-05-22 17:28:28 +0530

    bgslider

--- src/assets/BackgroundSlider.jsx
+++ src/assets/BackgroundSlider.jsx
@@ -1,39 +1,35 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import bgImage1 from '../images/bg.jpg';
-import bgImage2 from '../images/silderOne.png';
+import React, { useState, useEffect } from 'react';
+import slideOne from '../images/silderOne.png';
+import slideTwo from '../images/sliderTwo.jpg';
+import slideThree from '../images/sliderThree.jpg';
 
+const slides = [slideOne, slideTwo, slideThree];
+const slideTexts = [''];
 
 const BackgroundSlider = () => {
-  const settings = {
-    dots: false,
-    arrows: false,
-    infinite: true,
-    speed: 2000,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    fade: true,
-    cssEase: 'linear'
-  };
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
-    <div className="">
-      <Slider {...settings}>
-        <div>
-          <img src={bgImage2} alt="Image 1" className="" />
-        </div>
-        <div>
-          <img src={bgImage1} alt="Image 2" className="" />
-        </div>
-        
-      </Slider>
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="absolute inset-0 flex items-center justify-center">
-        shghj
+    <div className="relative h-full">
+      {slides.map((slide, index) => (
+        <img
+          key={index}
+          src={slide}
+          alt={`Slide ${index + 1}`}
+          className={`absolute inset-0 h-full w-full opacity-0 transition-opacity ${
+            index === currentSlide ? 'opacity-100' : ''
+          }`}
+        />
+      ))}
+      <div className="absolute inset-0 flex items-center justify-center text-white text-4xl font-bold">
+        {slideTexts[currentSlide]}
       </div>
     </div>
   );
