ac0cea04737a6248d54862c1f62b875e4fe0a8cd
UmairShukri <123258035+UmairShukri@users.noreply.github.com>
2023-05-22 11:20:07 +0530

    fgf

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import CountryForm from './Countryform'
 import bgImage from '../images/bg.jpg'
-
+import slideOne from '../images/silderOne.png'
+import BackgroundSlider from '../assets/BackgroundSlider'
 
 
 export default function Calculator() {
   return (
-    <div className="flex flex-column  justify-center">
+    
+    <div className="flex flex-column ">
+
+
+      
       <div className="  flex items-start justify-center h-screen w-80 p-4 border-2 border-gray-500 bg-white bg-cover bg-center"
        style={{ backgroundImage: `url(${bgImage})`,
        opacity: '0.9', }} >
@@ -14,7 +19,11 @@ export default function Calculator() {
       
         <CountryForm/>
       </div>
-      <div className='bg-white'>
+      
+      <div className= "slider bg-"  >
+       
+
+        All relationships begins with trust.
         
       
         
