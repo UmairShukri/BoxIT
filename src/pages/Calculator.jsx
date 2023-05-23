2e091583dc38c263098ea462e4f0b93d9f338e18
UmairShukri <123258035+UmairShukri@users.noreply.github.com>
2023-05-23 17:22:19 +0530

    nav

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -6,11 +6,12 @@ import Navbar from '../components/Navbar';
 
 const Calculator = () => {
   return (
-    
-    <div className="flex flex-col">
+    <div className="relative flex flex-col">
+      <div className="relative">
+        <Navbar />
+      </div>
       <div className="boxit relative flex items-center justify-center m-8">
         <div className="absolute inset-0 z-0">
-          <Navbar/>
           <BackgroundSlider />
         </div>
         <div className="relative z-10 p-10">
@@ -33,17 +34,23 @@ const Calculator = () => {
         </p2>
       </div>
 
-      <div className="flex flex-col items-center justify-center p-8 space-y-8 sm:flex-row sm:space-x-20 sm:space-y-0">
+      <div className="tm-9 flex flex-col items-center justify-center p-8 space-y-8 sm:flex-row sm:space-x-20 sm:space-y-0">
         <div className="w-full sm:w-96 pl-2 bg-cover bg-center w-683 h-819 bg-gray-200 rounded-[32px]">
           <h1 className="text-2xl font-bold text-center mb-4 pt-8">GET A QUOTE</h1>
           <CountryForm />
         </div>
         <div className="flex flex-col items-center">
-          <h3 className="text-xl ">We will help you to shop for you around the world.</h3>
-          <button type="button" className="bg-lime-300 text-black m-8 px-4 py-2 rounded-md hover:bg-blue-600 rounded-lg font-bold">
+          <h3 className="text-xl">We will help you to shop for you around the world.</h3>
+          <button
+            type="button"
+            className="bg-lime-300 text-black m-8 px-4 py-2 rounded-md hover:bg-blue-600 rounded-lg font-bold"
+          >
             Shopping Request
           </button>
-          <button type="button" className="bg-lime-300 text-black m-8 px-4 py-2 rounded-md hover:bg-blue-600 rounded-lg font-bold">
+          <button
+            type="button"
+            className="bg-lime-300 text-black m-8 px-4 py-2 rounded-md hover:bg-blue-600 rounded-lg font-bold"
+          >
             Portal Login
           </button>
         </div>
