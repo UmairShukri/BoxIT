e8bec0b2866bce7eba185f3fd093c892416540a3
UmairShukri <123258035+UmairShukri@users.noreply.github.com>
2023-07-20 19:47:49 +0530

    lkr

--- client/src/components/Price.jsx
+++ client/src/components/Price.jsx
@@ -7,7 +7,7 @@ export default function Price({ visible, onClose, shippingPrice }) {
     <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50 backdrop-blur-sm">
       <div className="p-4 w-full max-w-md bg-gray-200 rounded-lg">
         <h1 className="text-center font-bold text-xl">TOTAL AMOUNT</h1>
-        <p className="text-center">Shipping Price: {shippingPrice}</p>
+        <p className="text-center">Shipping Price: {shippingPrice} LKR</p>
         <button onClick={onClose} className="block mx-auto mt-4 px-4 py-2 bg-green-500 text-white rounded-md font-bold">
           Close
         </button>
