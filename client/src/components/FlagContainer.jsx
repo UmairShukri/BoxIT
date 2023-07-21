17ae5d766b901a92d7cfc684f0374f272be0410b
UmairShukri <123258035+UmairShukri@users.noreply.github.com>
2023-07-21 17:28:25 +0530

    footerandcountryflags

--- client/src/components/FlagContainer.jsx
+++ client/src/components/FlagContainer.jsx
@@ -9,10 +9,25 @@ export default function FlagContainer() {
     <div className='border-4 bg-gray-200 rounded-2xl flex flex-col p-6 m-5'>
       <h1 className='text-center font-bold'>Countries we Export to</h1>
       <div className='flex flex-wrap justify-center items-center space-x-4 space-y-4'>
-        <img className='max-w-xs' src={India} alt='india' />
-        <img className='max-w-xs' src={China} alt='china' />
-        <img className='max-w-xs' src={Male} alt='maldives'/>
-        <img className='max-w-xs' src={Dubai} alt='dubai' />
+        <div className='flex flex-col items-center text-center'>
+          <img className='max-w-xs' src={India} alt='india' />
+          <span className='font-bold text-sm'>INDIA</span>
+        </div>
+
+        <div className='flex flex-col items-center text-center'>
+          <img className='max-w-xs' src={China} alt='china' />
+          <span className='font-bold text-sm pt-8'>CHINA</span>
+        </div>
+
+        <div className='flex flex-col items-center text-center'>
+          <img className='max-w-xs' src={Male} alt='maldives'/>
+          <span className='font-bold text-sm'>MALDIVES</span>
+        </div>
+
+        <div className='flex flex-col items-center text-center'>
+          <img className='max-w-xs' src={Dubai} alt='dubai' />
+          <span className='font-bold text-sm'>DUBAI</span>
+        </div>
       </div>
     </div>
   );
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,23 +1,27 @@
 import React from "react";
 import ItemsContainer from "./ItemsContainer";
 import SocialIcons from "./SocialIcons";
-import { Icons } from "./Menus";
+import Icon from '../images/boxitLogo.png';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-300 text-black h-auto m-8 rounded-lg "> 
-     
-      <div
-        className="flex flex-row  text-center pt-2 text-black-400 text-sm pb-8 flex space-x-5 "
-      >
-        <div className="flex flex-col ">
+    <footer className="bg-gray-300 text-black h-auto m-6 rounded-lg"> 
+      <div className="flex flex-row justify-between text-center pt-2 text-black-400 text-sm pb-8 flex space-x-5 space-y-5 m-9  ml-10">
+        <div className="flex flex-col font-bold font-serif flex-1">
           <div>About</div>
           <div>Contact</div>
           <div>Calculator</div>
-
         </div>
-        <div className="flex ">© 2023. All rights reserved.</div>
-        
+        <div className="font-bold font-mono flex-1">  
+          <div>FAQs</div>
+          <div>Privacy Policy</div>
+          <div>Terms and Conditions</div>
+        </div>
+
+        <div className="flex flex-col items-end flex-1 px-9">
+          <img src={Icon} alt="logo" className="h-8 mr-1 md:h-8 md:mr-0 md:ml-2" />
+          © 2023. All rights reserved.
+        </div>
       </div>
     </footer>
   );
